eafcd5be7b73a728e1eeab73d032eb4906a77fac
realib1 <ibeevpersie@gmail.com>
2022-08-31 01:22:57 +0000

    Changes to be committed:
        new file:   .gitignore
    	new file:   assets/images/africa.webp
    	new file:   index.html
    	new file:   javascript.svg
    	new file:   main.js
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/vite.svg
    	new file:   style.css

---
+++ main.js
@@ -0,0 +1,46 @@
+// Drop down list
+
+document.addEventListener('click', e => {
+    const dropDownButton = e.target.matches("[data-dropdown-button]")
+    if (!dropDownButton && e.target.closest("[data-dropdown]") != null) return;
+
+    let currentDropdown;
+    if (dropDownButton) {
+        currentDropdown = e.target.closest("[data-dropdown]")
+        currentDropdown.classList.toggle('active')
+    
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove("active")
+    })
+})
+
+// Navigation hamburger menu toggle
+
+
+const barMenu = document.querySelector(".bars-menu");
+const navMenu = document.querySelector(".nav-list");
+const navLink = document.querySelectorAll(".nav-link");
+const main = document.getElementsByClassName('main')
+barMenu.addEventListener("click", openNav);
+navLink.forEach((n) => n.addEventListener("click", closeNav));
+
+function openNav() {
+  barMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+
+function closeNav() {
+  barMenu.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+window.addEventListener("click", function(e) {
+  if (e.target === main) {
+    barMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+})
